982914fff7e7c422a46a41a037c5d3ad7c8d987a
rogu3bear <vats-springs0m@icloud.com>
2026-05-19 18:30:10 -0500

    Use generic template hygiene scan

--- scripts/check-template.sh
+++ scripts/check-template.sh
@@ -18,7 +18,11 @@ done
 
 echo "== personal data scan"
 if rg -n \
-  -e 'adapteros|mlnavigator|auchshop|jkca|donella|james|scopic|/Users/star|privaterelay|icloud' \
+  -e '/Users/' \
+  -e 'privaterelay' \
+  -e 'icloud' \
+  -e 'CLOUDFLARE_ACCOUNT_ID=[A-Za-z0-9]' \
+  -e 'CLOUDFLARE_API_TOKEN=[A-Za-z0-9]' \
   "${ROOT_DIR}" \
   --glob '!target/**' \
   --glob '!var/**' \
